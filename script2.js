288c32efc61abd72f37d6ddf34ad714382a10789
Miroslav-student <147755633+Miroslav-student@users.noreply.github.com>
2024-01-28 17:42:03 +0000

    Final

--- script2.js
+++ script2.js
@@ -37,7 +37,7 @@ function updateClock() {
 function updateDate() {
   const currentDate = new Date();
   const day = currentDate.getDate();
-  const month = (currentDate.getMonth() + 1).toString().padStart(2, "0"); // Додаємо нуль перед однозначним місяцем
+  const month = (currentDate.getMonth() + 1).toString().padStart(2, "0");
   const year = currentDate.getFullYear();
   const formattedDate = `${day}.${month}.${year}`;
   dateElem.textContent = formattedDate;
